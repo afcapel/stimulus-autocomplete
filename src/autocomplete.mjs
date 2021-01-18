efcbf856bab3a0dfd6ed9d2f4ed182d45189e89f
glaszig <glaszig@gmail.com>
2021-01-18 14:40:06 -0300

    allow more elaborate markup with selective input value

--- src/autocomplete.mjs
+++ src/autocomplete.mjs
@@ -1,6 +1,18 @@
 import { Controller } from "stimulus"
 import debounce from "lodash.debounce"
 
+const extractTextValue = el => {
+  if (el.hasAttribute("aria-labeledby")) {
+    return el.getAttribute("aria-labeledby").split(" ")
+      .map(id => document.getElementById(id))
+      .filter(el => el !== null)
+      .map(el => el.textContent.trim())
+      .join(" ")
+  } else {
+    return el.textContent.trim()
+  }
+}
+
 export default class extends Controller {
   static targets = ["input", "hidden", "results"]
   static values = {
@@ -136,7 +148,7 @@ export default class extends Controller {
       return
     }
 
-    const textValue = selected.textContent.trim()
+    const textValue = extractTextValue(selected)
     const value = selected.getAttribute("data-autocomplete-value") || textValue
     this.inputTarget.value = textValue
 
